aec9f2569b933247a6dbfe191db1e98cca2d73e9
Tionna Wilson Pierce <tionna.wilsonpierce@gmail.com>
2018-03-19 13:14:09 -0400

    add config

---
+++ frontend/components/loginPage.jsx
@@ -0,0 +1,23 @@
+{
+  "development": {
+    "username": "tionnawilsonpierce",
+    "password": null,
+    "database": "instagram2.0",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "tionnawilsonpierce",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "tionnawilsonpierce",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
